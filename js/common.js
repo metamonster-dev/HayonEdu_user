d51eaac8462e212bdfd33bef7234bb786258f931
hdy86 <dyhong86@naver.com>
2024-09-10 18:41:39 +0900

    feat: 회사소개 - 화연과학 스토리, 사훈 및 CI소개, 회사연혁, 오시는길 퍼블, 보도자료 퍼블 진행중

--- js/common.js
+++ js/common.js
@@ -1,15 +1,16 @@
-$(document).ready(function() {
-    //PC에서 뷰포트 조정시 화면 리로드
-    if (!/Mobi|Android/i.test(navigator.userAgent)) {
-        window.addEventListener('resize', function() {
-            location.reload();
-        });
-    }
+$(document).ready(function () {
+  //PC에서 뷰포트 조정시 화면 리로드
+  // if (!/Mobi|Android/i.test(navigator.userAgent)) {
+  //     window.addEventListener('resize', function() {
+  //         location.reload();
+  //     });
+  // }
 
+  $(".label_chk_wrap").click(function () {
+    // label 안에 있는 checkbox를 찾아서 체크 상태를 반전
+    var checkbox = $(this).find('input[type="checkbox"]');
+    checkbox.prop("checked", !checkbox.prop("checked"));
+  });
 
-    $('.label_chk_wrap').click(function() {
-        // label 안에 있는 checkbox를 찾아서 체크 상태를 반전
-        var checkbox = $(this).find('input[type="checkbox"]');
-        checkbox.prop('checked', !checkbox.prop('checked'));
-    });
+  AOS.init();
 });
